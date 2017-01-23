6693263f4a74f1a7efdb68f090e77ffe9ed72abb
solovei.lm <solovei.lm@gmail.com>
2017-01-24 01:19:20 +0200

    wip creat site_monitoring

--- block-parameter.php
+++ block-parameter.php
@@ -9,7 +9,12 @@
 <li><input type="checkbox" id="checkbox4"/><label fot="checkbox4">От Я до А</label></li>
 
 </ul>
-
+<form action="select.php" method="POST"><p><select size='3'name="">
+<option disabled>Без сортировки</option>
+<option value="">По возростанию цены</option>
+<option value="">По убыванию цены</option>
+<option value="">По алфавиту</option>
+</select></p></form>
 <center><input type="submit" name="submit" id="button-param-search" value="" />Найти</center>
 </form>
 
